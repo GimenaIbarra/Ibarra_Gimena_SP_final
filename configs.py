433a8ff150303ee36b22a97b7bc1ecd6fb0353c6
GimenaIbarra <ibarra.gimena.abril@gmail.com>
2026-02-24 23:07:53 -0300

    Configs

---
+++ configs.py
@@ -0,0 +1,5 @@
+SIZE_WINDOW = (820, 480)
+DIFICUTYS = [5,6,7]
+CLUES = {"descubrir continente": 1, "descubrir letra": 1, "descubrir comida": 1}
+TRYS = 6
+POINTS_FOR_DIFICULTY = [[5, 50, 10], [6, 80, 12], [7, 100, 20]]
